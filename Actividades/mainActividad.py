5b07a49d85e6992d5883de5d982c02f3f6653c45
Luis Valero <67386033+LuisValero2000@users.noreply.github.com>
2020-11-24 09:10:57 -0400

    Add files via upload

--- Actividades/mainActividad.py
+++ Actividades/mainActividad.py
@@ -4,5 +4,7 @@ import tablero
 G = generador.Generador()
 tablero = tablero.Tablero()
 tablero.set_tamano(5,5)
+tablero.set_matriz()
 p1 = G.generar_pieza()
 p1.mostrar()
+tablero.imprimir_matriz()
--- Actividades/tablero.py
+++ Actividades/tablero.py
@@ -1,3 +1,4 @@
+import pieza
 class Tablero:
 
     def set_tamano(self,tamano_i,tamano_j):
@@ -6,3 +7,24 @@ class Tablero:
 
     def get_tamano(self):
         return self._tamano
+
+    def set_matriz(self):
+        self._filas = []
+        self._columnas = []
+        for i in range(self._tamano_i):
+            for i in range(self._tamano_j):
+                self._columnas.append(-1)
+            self._filas.append(self._columnas)
+            self._columnas = []
+    
+    def get_matriz(self):
+        return self._filas
+    
+    def imprimir_matriz(self):
+        print(self.get_matriz())
+
+    def generar_tablero(self,tamano_i,tamano_j):
+        t = Tablero()
+        t.set_tamano(tamano_i,tamano_j)
+
+    #def ubicar_pieza(pieza, region):
